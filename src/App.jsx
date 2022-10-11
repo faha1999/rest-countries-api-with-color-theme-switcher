a4bc40bccdc6b001f2b51cf453f7d6e5df96d15c
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-10-11 19:09:22 +0600

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Header } from './components/header/Header';
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Toggle from './partials/Toggler';
+
+export const Header = () => {
+  return (
+    <>
+      <Toggle />
+    </>
+  );
+};
